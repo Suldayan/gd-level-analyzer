e4d9c7552b8662b06a4c8992e2af652666442d20
Timothy <timothysuldayan1@gmail.com>
2025-07-25 19:24:15 -0700

    configured spring boot backend

---
+++ gd-level-analyzer-backend/src/main/java/com/example/gd_level_analyzer_backend/GdLevelAnalyzerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gd_level_analyzer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdLevelAnalyzerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdLevelAnalyzerBackendApplication.class, args);
+	}
+
+}
---
+++ gd-level-analyzer-backend/src/test/java/com/example/gd_level_analyzer_backend/GdLevelAnalyzerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gd_level_analyzer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdLevelAnalyzerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
